46838c4ee6ddc3d35f5357ca4e36d0627c89b8b0
김성훈 <kshmarine@gmail.com>
2018-10-30 00:12:09 +0900

    API 데이터 불러오기 준비

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,9 +1,12 @@
 <template>
   <div>
     Board
-    <div>bid: {{bid}}</div>
-    <router-link :to="`/b/${bid}/c/1`">Card 1</router-link>
-    <router-link :to="`/b/${bid}/c/2`">Card 2</router-link>
+    <div v-if="loading">loading board ...</div>
+    <div v-else>
+      <div>bid: {{bid}}</div>
+      <router-link :to="`/b/${bid}/c/1`">Card 1</router-link>
+      <router-link :to="`/b/${bid}/c/2`">Card 2</router-link>
+    </div>
     <hr />
     <router-view></router-view>
   </div>
@@ -11,12 +14,23 @@
 
 <script>
 export default {
-  created() {
-    this.bid = this.$route.params.bid
-  },
   data() {
     return {
-      bid: 0
+      bid: 0,
+      loading: false
+    }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      // backend에서 데이터를 호출한다
+      this.loading = true
+      setTimeout(() => {
+        this.bid = this.$route.params.bid
+        this.loading = false
+      }, 500)
     }
   }
 }
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
     Card
-    <div>cid: {{ cid }}</div>
+    <div v-if="loading">loading card ...</div>
+    <div v-else>
+      <div>cid: {{ cid }}</div>
+    </div>
   </div>
 </template>
 
@@ -9,16 +12,26 @@
 export default {
   data() {
     return {
-      cid: 0
+      cid: 0,
+      loading: false
     }
   },
   watch: {
-    '$route'() {
-      this.cid = this.$route.params.cid
+    // 감시자에 handler와 immediate속성을 통해 라우터 변경에 따라 바로 실행할 수 있다.
+    // 기존에 있던 created훅에서 실행하던 구문의 중복을 제거할 수 있다. (여기서는 created된다는 말은 route됐다는 말이니..)
+    '$route': {
+      handler: 'fetchData',
+      immediate: true
     }
   },
-  created() {
-    this.cid = this.$route.params.cid
+  methods: {
+    fetchData() {
+      this.loading = true
+      setTimeout(() => {
+        this.cid = this.$route.params.cid
+        this.loading = false
+      }, 500)
+    }
   }
 }
 </script>

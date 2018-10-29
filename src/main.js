6efe1ac507b1bbc968ec30e03da33eed2d0de303
김성훈 <kshmarine@gmail.com>
2018-10-29 23:13:02 +0900

    vue-router

--- src/main.js
+++ src/main.js
@@ -1,22 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
 
 const Login = { template: '<div>Login Page</div>' }
+const NotFound = { template: '<div>Page not found</div>' }
 
-const routes = {
-  '/': App,
-  '/login': Login
-}
+const router = new VueRouter({
+  mode: 'history',  // history API를 사용하겠다. (hashbang대신)
+  routes: [
+    { path: '/', component: App},
+    { path: '/login', component: Login},
+
+    // router는 선언 순서대로 매핑하기 때문에 이부분은 최종적으로 위에서 매핑하지 못한 경로가 매핑됨
+    { path: '*', component: NotFound}
+  ]
+})
 
 new Vue({
   el: '#app',
-  computed: {
-    Vuecomponent() {
-      return routes[window.location.pathname] || 
-      { template: '<div>Page not found</div>' }
-    }
-  },
-  render(h) {
-    return h(this.Vuecomponent)
-  }
+  router,
+  render: h => h({ template: '<router-view></router-view>' })
 })

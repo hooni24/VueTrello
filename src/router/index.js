8743f04497963bf760c21c0f9175b5fd1d453062
김성훈 <kshmarine@gmail.com>
2018-10-29 23:50:15 +0900

    동적 라우트 매칭 적용 (파라미터 사용)

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Home from '../components/Home'
 import Login from '../components/Login'
 import NotFound from '../components/NotFound'
+import Board from '../components/Board'
 
 Vue.use(VueRouter)
 
@@ -11,6 +12,7 @@ const router = new VueRouter({
   routes: [
     { path: '/', component: Home},
     { path: '/login', component: Login},
+    { path: '/b/:bid', component: Board}, // :bid를 통해 해당부분을 변수로 받을 수 있다
 
     // router는 선언 순서대로 매핑하기 때문에 이부분은 최종적으로 위에서 매핑하지 못한 경로가 매핑됨
     { path: '*', component: NotFound}

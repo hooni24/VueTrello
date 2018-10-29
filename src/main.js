e953ffd26ac44135dbd964cc64d3455612277380
김성훈 <kshmarine@gmail.com>
2018-10-29 23:15:53 +0900

    router 리팩토링 (분리)

--- src/main.js
+++ src/main.js
@@ -1,22 +1,5 @@
 import Vue from 'vue'
-import App from './App.vue'
-import VueRouter from 'vue-router'
-
-Vue.use(VueRouter)
-
-const Login = { template: '<div>Login Page</div>' }
-const NotFound = { template: '<div>Page not found</div>' }
-
-const router = new VueRouter({
-  mode: 'history',  // history API를 사용하겠다. (hashbang대신)
-  routes: [
-    { path: '/', component: App},
-    { path: '/login', component: Login},
-
-    // router는 선언 순서대로 매핑하기 때문에 이부분은 최종적으로 위에서 매핑하지 못한 경로가 매핑됨
-    { path: '*', component: NotFound}
-  ]
-})
+import router from './router' // index인 경우는 생략가능
 
 new Vue({
   el: '#app',

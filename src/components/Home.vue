3589a91dfa2e47b1bf903a6ad0dca9d788961fc7
firepizza <dev.firepizza@gmail.com>
2018-10-30 20:09:10 +0900

    axios 적용

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,6 +3,9 @@
     Home
     <div>
       Board List:
+      <div v-if="loading">Loading...</div>
+      <dir v-else>API result : <pre>{{ apiRes }}</pre></dir>
+      <div v-if="error"><pre>{{ error }}</pre></div>
       <ul>
         <li>
           <router-link to="/b/1">Board 1</router-link>
@@ -16,8 +19,35 @@
 </template>
 
 <script>
-export default {
+import axios from 'axios'
 
+export default {
+  data() {
+    return {
+      loading: false,
+      apiRes: '',
+      error: ''
+    }
+  },
+  created() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      this.loading = true
+
+      axios.get('http://localhost:3000/health')
+      .then(res => {
+        this.apiRes = res.data
+      })
+      .catch(res => {
+        this.error = res.response.data
+      })
+      .finally(() => {
+        this.loading = false
+      })
+    }
+  }
 }
 </script>
 

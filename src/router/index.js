a2c90ae0c2f2c87466aa8cb4628ab4cc4c7fb314
firepizza <dev.firepizza@gmail.com>
2018-10-30 21:07:26 +0900

    navigation-guard 적용

--- src/router/index.js
+++ src/router/index.js
@@ -8,16 +8,23 @@ import Card from '../components/Card'
 
 Vue.use(VueRouter)
 
+// to: 현재경로
+const requireAuth = (to, from, next) => {
+  const isAuth = localStorage.getItem('token')
+  const loginPath = `/login?rPath=${encodeURIComponent(to.path)}`
+  isAuth ? next() : next(loginPath)
+}
+
 const router = new VueRouter({
   mode: 'history',  // history API를 사용하겠다. (hashbang대신)
   routes: [
-    { path: '/', component: Home},
+    { path: '/', component: Home, beforeEnter: requireAuth },
     { path: '/login', component: Login},
 
     // :bid를 통해 해당부분을 변수로 받을 수 있다
-    { path: '/b/:bid', component: Board, children: [ 
+    { path: '/b/:bid', component: Board, beforeEnter: requireAuth, children: [ 
       // children을 사용해 중첩라우팅 구현
-      { path: 'c/:cid', component: Card}
+      { path: 'c/:cid', component: Card, beforeEnter: requireAuth }
     ] },
 
     // router는 선언 순서대로 매핑하기 때문에 이부분은 최종적으로 위에서 매핑하지 못한 경로가 매핑됨
